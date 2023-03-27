02e18c0b7228cba56a106d5cdff0bce677fb17be
Matthew Pan <matthewdukepan@gmail.com>
2023-03-27 15:23:45 -0400

    working

--- QuestionGPTService/ReadMe.md
+++ QuestionGPTService/ReadMe.md
@@ -1,19 +1,20 @@
-# How to Run
-
 [Quick flask guide](https://flask.palletsprojects.com/en/2.2.x/quickstart/)
+
+# Prerequisites
+
+- Install Python
+- Install Flask `pip install Flask`
+- Install RabbitMQ `pip install pika`
+- Install dotenv to enable Environment Variables `pip install python-dotenv`
+
+# How to Run
 
 To run the application, use the flask command or python -m flask. You need to tell the Flask where your application is with the --app option.
 
-- Run RabbitMQ locally: `docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.11-management`
-
-```
-$ flask --app app.py run
- * Serving Flask app 'hello'
- * Running on http://127.0.0.1:5000 (Press CTRL+C to quit)
-```
-
-- Alternatively: `py -m flask --app app.py run`
-- To run in debug mode (automatic changes): `py -m flask --app app run --debug`
+1. Run RabbitMQ locally: `docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.11-management`
+2. `flask --app app.py run` (`py -m flask --app app.py run`)
+   - To run in debug mode (automatic changes): `py -m flask --app app run --debug`
+3. Go to root of localhost site to start the RabbitMQ connection (http://127.0.0.1:5000)
 
 RabbitMQ dashboard to manage messages:
 
--- QuestionGPTService/app.py
+++ QuestionGPTService/app.py
@@ -1,4 +1,6 @@
 '''
+RabbitMQ message formats:
+
 Receiving message format:
 - number: number of questions to generate
 - text: text to generate questions from
@@ -8,44 +10,50 @@ Sending message format:
 - id: unique identifier for quiz
 - number: number of questions generated
 - results: list of question-answer pairs
+
+Test:
+{
+    "id": 1,
+    "number": 3,
+    "text": "Data structures and algorithms. Medium difficulty."
+}
+{
+    "id": 2,
+    "number": 3,
+    "text": "Vegetables and fruits. Medium difficulty."
+}
 '''
 import os
 import json
 import pika
 import requests
-from flask import Flask, request
+from flask import Flask
 import threading
+from utils import MOCK_RESPONSE, gpt_prompt
 
 app = Flask(__name__)
+env = os.environ.get("ENVIRONMENT")
 
 # RabbitMQ configurations
-RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "localhost")
+RABBITMQ_HOST = os.environ.get("DEV_RABBITMQ_HOST", "localhost") if env == "dev" else os.environ.get("PROD_RABBITMQ_HOST")
 INPUT_QUEUE = os.environ.get("INPUT_QUEUE", "input_queue")
 OUTPUT_QUEUE = os.environ.get("OUTPUT_QUEUE", "output_queue")
 
 # ChatGPT API configurations
 CHATGPT_API_KEY = os.environ.get("OPENAI_SECRET_KEY")
 CHATGPT_API_ENDPOINT = os.environ.get("CHATGPT_API_ENDPOINT", "https://api.openai.com/v1/chat/completions")
-# https://api.openai.com/v1/engines/davinci-codex/completions
-MOCK_RESPONSE = '''Question 1: What is a binary search tree?
-Answer: A binary search tree is a binary tree data structure where each node has at most two child nodes and the value of each node is greater than or equal to the values in its left subtree and less than or equal to the values in its right subtree.
-
-Question 2: What is the time complexity of searching for an element in a binary search tree?
-Answer: The time complexity of searching for an element in a binary search tree is O(log n) on average, where n is the number of nodes in the tree.
-
-Question 3: What is the difference between a stack and a queue?
-Answer: A stack is a data structure that follows the Last-In-First-Out (LIFO) principle, where the last element added to the stack is the first one to be removed. A queue, on the other hand, follows the First-In-First-Out (FIFO) principle, where the first element added to the queue is the first one to be removed.'''
 
 headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {CHATGPT_API_KEY}"
 }
 
+# RabbitMQ singleton channel
+global GLOBAL_RMQ_CHANNEL
+GLOBAL_RMQ_CHANNEL = None
 
 def chatgpt_request(prompt, nb_questions, max_tokens=200):
-    final_prompt = f"Generate a quiz of {nb_questions} questions from the following prompt:\n{prompt}\n\
-        Also generate answers for each of the questions generated from the prompt. Let the formatting be 'Question 1: ...'\
-              then a new line, and then with its answer as 'Answer: ...' for each of the questions. Each block of question and answer is separated with 2 new lines."
+    final_prompt = gpt_prompt(prompt, nb_questions)
     data = {
         "model": "gpt-3.5-turbo-0301",
         "messages": [
@@ -53,16 +61,19 @@ def chatgpt_request(prompt, nb_questions, max_tokens=200):
         ],
         "max_tokens": max_tokens,
         "n": 1,
-        "temperature": 0.7,
+        "temperature": 0.8,
     }
 
-    #response = requests.post(CHATGPT_API_ENDPOINT, headers=headers, json=data)
-    #print("Response: ", response.json())
-    #response.raise_for_status()
-    #return response.json()["choices"][0]["message"]["content"].strip()
-    return MOCK_RESPONSE
+    if env == "dev":
+        return MOCK_RESPONSE
+
+    response = requests.post(CHATGPT_API_ENDPOINT, headers=headers, json=data)
+    print("Response: ", response.json())
+    response.raise_for_status()
+    return response.json()["choices"][0]["message"]["content"].strip()
 
 def rabbitmq_connection():
+    global GLOBAL_RMQ_CHANNEL
     print("Starting RabbitMQ connection.")
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
     if connection.is_open:
@@ -71,11 +82,10 @@ def rabbitmq_connection():
         print(f"Failed to connect to RabbitMQ at {RABBITMQ_HOST}")
         return None
     print("Creating channel")
-    channel = connection.channel()
+    GLOBAL_RMQ_CHANNEL = connection.channel()
     print("Creating queues")
-    channel.queue_declare(queue=INPUT_QUEUE)
-    channel.queue_declare(queue=OUTPUT_QUEUE)
-    return channel
+    GLOBAL_RMQ_CHANNEL.queue_declare(queue=INPUT_QUEUE)
+    GLOBAL_RMQ_CHANNEL.queue_declare(queue=OUTPUT_QUEUE)
 
 def parse_qa_pairs(text):
     questions = text.split("\n\n")
@@ -90,37 +100,43 @@ def parse_qa_pairs(text):
     return formatted_questions
 
 def callback(ch, method, properties, body):
-    message = json.loads(body)
-    quiz_id = message["id"]
-    number = message["number"]
-    text = message["text"]
+    try:
+        message = json.loads(body)
+        print(message)
+        quiz_id = message["id"]
+        number = message.get("number",0)
+        text = message.get("text","")
 
-    generated_text = chatgpt_request(text, number)
-    print('Generated text---\n',generated_text)
-    # Parse questions and answers
-    formatted_qa = parse_qa_pairs(generated_text)
+        generated_text = chatgpt_request(text, number)
+        print('Generated text---\n',generated_text)
+        # Parse questions and answers
+        formatted_qa = parse_qa_pairs(generated_text)
 
-    # Add message back to the queue
-    response_message = {
-        "id": quiz_id,
-        "number": number,
-        "results": formatted_qa
-    }
-    print('Response message---\n',response_message)
+        # Add message back to the queue
+        response_message = {
+            "id": quiz_id,
+            "number": number,
+            "results": formatted_qa
+        }
+        print('Response message---\n',response_message)
 
-    ch.basic_publish(exchange="", routing_key=OUTPUT_QUEUE, body=json.dumps(response_message))
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-    print('Finished callback')
+        ch.basic_publish(exchange="", routing_key=OUTPUT_QUEUE, body=json.dumps(response_message))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        print('Finished callback')
+    except Exception as e:
+        print("Error processing message: ", quiz_id, e)
+        ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
 
 def start_consuming():
-    channel = rabbitmq_connection()
-    if channel is None:
+    global GLOBAL_RMQ_CHANNEL
+    rabbitmq_connection()
+    if GLOBAL_RMQ_CHANNEL is None:
         print("Error connecting to input queue. Exiting...")
         return
-    else: print('RabbitMQ connection established and channel created. Starting consuming...')
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue=INPUT_QUEUE, on_message_callback=callback)
-    channel.start_consuming()
+    else: print('RabbitMQ connection established and channel created. Starting to consume...')
+    GLOBAL_RMQ_CHANNEL.basic_qos(prefetch_count=1)
+    GLOBAL_RMQ_CHANNEL.basic_consume(queue=INPUT_QUEUE, on_message_callback=callback)
+    GLOBAL_RMQ_CHANNEL.start_consuming()
 
 @app.route("/test")
 def test():
@@ -131,23 +147,47 @@ def test():
     print(finalr)
     return finalr
 
+@app.route('/outputq')
+def list_outputq():
+    temp_connect = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+    temp_channel = temp_connect.channel()
+    temp_channel.queue_declare(queue=OUTPUT_QUEUE)
+    msgs = []
+    while True:
+        method_frame, header_frame, body = temp_channel.basic_get(queue=OUTPUT_QUEUE, auto_ack=False)
+        if method_frame:
+            msgs.append(json.loads(body))
+        else: break
+    temp_channel.cancel()
+    temp_channel.close()
+    return msgs
+
+@app.route('/inputq')
+def list_inputq():
+    temp_connect = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+    temp_channel = temp_connect.channel()
+    temp_channel.queue_declare(queue=INPUT_QUEUE)
+    msgs = []
+    while True:
+        method_frame, header_frame, body = temp_channel.basic_get(queue=INPUT_QUEUE, auto_ack=False)
+        if method_frame:
+            msgs.append(json.loads(body))
+        else: break
+    temp_channel.cancel()
+    temp_channel.close()
+    return msgs
+
 @app.route("/")
 def home():
-    return "Hello World!"
+    return "Question GPT Service is now running."
 
-# @app.route("/start")
-# def start():
-#     channel = rabbitmq_connection()
-#     if channel is None:
-#         print("Error connecting to input queue. Exiting...")
-#         return
-#     channel.basic_qos(prefetch_count=1)
-#     channel.basic_consume(queue=INPUT_QUEUE, on_message_callback=callback)
-#     channel.start_consuming()
-
-    return {"status": "success"}
-
-if __name__ == "__main__":
+# Go to root home page to start the connection with rabbitmq
+@app.before_first_request
+def startup():
+    print('Starting RabbitMQ thread')
     rabbitmq_thread = threading.Thread(target=start_consuming)
     rabbitmq_thread.start()
-    app.run(host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    if env == "dev":
+        app.run(host="0.0.0.0", port=5000)
