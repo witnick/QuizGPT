1f83aad580fdd1d6708a84beed3aa5b0a93612f0
Stefan Russo <me@stefanrusso.ca>
2023-03-27 18:36:11 -0400

    Started Accountservice

---
+++ accountservice/src/main/java/com/infoguru/accountservice/AccountserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infoguru.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountserviceApplication.class, args);
+    }
+
+}
