85a491ecb58e29356b88ef8e17518aea2fb63576
Stefan Russo <me@stefanrusso.ca>
2023-04-04 19:42:50 -0400

    Fixed typo in Class name

--- accountservice/src/main/java/com/quizgpt/accountservice/messaging/MessageQueueConfig.java
+++ accountservice/src/main/java/com/quizgpt/accountservice/messaging/MessageQueueConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MessagQueueConfig {
+public class MessageQueueConfig {
 
     @Bean
     public ConnectionFactory connectionFactory() {
