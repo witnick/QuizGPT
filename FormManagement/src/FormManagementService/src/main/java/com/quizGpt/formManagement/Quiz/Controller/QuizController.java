3de6a18b83a12b528c10034d6f5bc7eab63ca7de
witnick <witnick2joseph@gmail.com>
2023-04-07 15:35:13 -0400

    Added end point support for question manipulation
    - removed extra controllers for questions & Quiz attempt
    - added logic for question in quiz controller

--- FormManagement/src/FormManagementService/src/main/java/com/quizGpt/formManagement/Quiz/Controller/QuizController.java
+++ FormManagement/src/FormManagementService/src/main/java/com/quizGpt/formManagement/Quiz/Controller/QuizController.java
@@ -1,7 +1,10 @@
 package com.quizGpt.formManagement.Quiz.Controller;
 
+import com.quizGpt.formManagement.Quiz.Dto.QuestionDto;
 import com.quizGpt.formManagement.Quiz.Dto.QuizDto;
+import com.quizGpt.formManagement.Quiz.Entity.Question;
 import com.quizGpt.formManagement.Quiz.Entity.Quiz;
+import com.quizGpt.formManagement.Quiz.Exception.QuestionNotFoundException;
 import com.quizGpt.formManagement.Quiz.Exception.QuizNotFoundException;
 import com.quizGpt.formManagement.Quiz.Repository.QuizRepository;
 import com.quizGpt.formManagement.Quiz.Services.IQuizService;
@@ -11,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -25,39 +29,88 @@ public class QuizController {
         this.quizService = quizService;
     }
 
+    ///Quiz
     @GetMapping("/quizzes")
-    public List<Quiz> all(){
-        List<Quiz> quizzes = this.quizService.getAllQuizzes();
+    public List<QuizDto> GetAllQuizzes(){
+        List<QuizDto> quizDtos = new ArrayList<QuizDto>();
+        List<Quiz> quizzes = this.quizService.GetAllQuizzes();
 
-        return quizzes;
+        for (Quiz quiz : quizzes) {
+            QuizDto quizDto = modelMapper.map(quiz, QuizDto.class);
+            quizDtos.add(quizDto);
+        }
+
+        return quizDtos;
     }
 
     @GetMapping("/quiz/{id}")
     public QuizDto GetQuiz(@PathVariable Long id) throws QuizNotFoundException {
-        Quiz quiz =  quizService.getQuizById(id);
+        Quiz quiz =  quizService.GetQuizById(id);
         var quizDto = modelMapper.map(quiz, QuizDto.class);
         return quizDto;
     }
 
     @PostMapping("/quiz")
-    public QuizDto createQuiz(@RequestBody QuizDto newQuiz){
+    public QuizDto CreateQuiz(@RequestBody QuizDto newQuiz){
         var quiz = modelMapper.map(newQuiz, Quiz.class);
         quiz.setCreatedAt(LocalDateTime.now());
         quiz.setUpdatedAt(LocalDateTime.now());
-        newQuiz = modelMapper.map(quizService.saveQuiz(quiz), QuizDto.class);
+        newQuiz = modelMapper.map(quizService.SaveQuiz(quiz), QuizDto.class);
         return newQuiz;
     }
 
     @PutMapping("/quiz")
-    public void updateQuiz(@RequestBody @NotNull QuizDto quizRequest) throws QuizNotFoundException {
-        Quiz quizEntity = quizService.getQuizById(quizRequest.getId());
+    public void UpdateQuiz(@RequestBody @NotNull QuizDto quizRequest) throws QuizNotFoundException {
+        Quiz quizEntity = quizService.GetQuizById(quizRequest.getId());
         var quizToUpdate = modelMapper.map(quizRequest, Quiz.class);
-        quizService.saveQuiz(quizToUpdate);
+        quizService.SaveQuiz(quizToUpdate);
     }
 
     @DeleteMapping("/quiz/{id}")
-    public void DeleteQuiz(@PathVariable Long id){
-        quizService.deleteQuiz(id);
+    public void DeleteQuiz(@PathVariable Long id) throws QuizNotFoundException {
+        quizService.DeleteQuiz(id);
     }
+
+    ///Questions
+    @GetMapping("/questions")
+    public List<QuestionDto> GetAllQuestions(){
+        List<QuestionDto> questionDtos = new ArrayList<>();
+        List<Question> questions = this.quizService.GetAllQuestions();
+
+        for (Question question : questions) {
+            QuestionDto questionDto = modelMapper.map(question, QuestionDto.class);
+            questionDtos.add(questionDto);
+        }
+
+        return questionDtos;
+    }
+
+    @GetMapping("/questions/{id}")
+    public QuestionDto GetQuestion(@PathVariable Long id) throws QuestionNotFoundException {
+        Question Question =  quizService.GetQuestionById(id);
+        var QuestionDto = modelMapper.map(Question, QuestionDto.class);
+        return QuestionDto;
+    }
+
+    @PostMapping("/questions")
+    public QuestionDto createQuestion(@RequestBody QuestionDto newQuestion) {
+        var Question = modelMapper.map(newQuestion, Question.class);
+        newQuestion = modelMapper.map(quizService.SaveQuestion(Question), QuestionDto.class);
+        return newQuestion;
+    }
+
+    @PutMapping("/questions")
+    public void updateQuestion(@RequestBody @NotNull QuestionDto QuestionRequest) throws QuestionNotFoundException {
+        Question QuestionEntity = quizService.GetQuestionById(QuestionRequest.getQuestionId());
+        var QuestionToUpdate = modelMapper.map(QuestionRequest, Question.class);
+        quizService.SaveQuestion(QuestionToUpdate);
+    }
+
+    @DeleteMapping("/questions/{id}")
+    public void DeleteQuestion(@PathVariable Long id) throws QuestionNotFoundException {
+        quizService.DeleteQuestion(id);
+    }
+
+    ///Quiz Attempt
 }
 
